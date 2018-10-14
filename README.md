919d28649f8917f1fe05756c501e0b0704671874
StreakSharpshot <jayvier13@gmail.com>
2018-10-14 05:56:33 -0500

    Revert "Updated to search for game paths"
    This reverts commit 8f5b22a181fa59507902a48ff7b4e7950ebb85b5.

--- README.md
+++ README.md
@@ -2,9 +2,17 @@
 This simple tool allows a user to quickly set up Steam Workshop mods in Starbound's sbinit.config for dedicated servers.
 
 ## Using:
-Simply run StarConfigurator.exe and it will automatically generate a new configuration file for Starbound.
+Simply edit the directories in `StarConfigurator.exe.config` to match the install location of Starbound and Steam workshop folder.
 
+If the application doesn't have file permissions, run it as Administrator.
 
-To do:
+### Example: 
+	<add key="StarboundDirectory" value="C:\Program Files (x86)\Steam\steamapps\common\Starbound\win64"/>
+	<add key="WorkshopFolder" value="C:\Program Files (x86)\Steam\steamapps\workshop\content\211820"/>
+	
+Notes:
 
-	Add command-line arguments for custom paths	
+	If there's a backslash at the end of your directory, delete it. Otherwise, the config file will not work,
+	and Starbound will throw a fit.
+	
+	211820 is Starbound's app ID. Always use this workshop folder.
--- StarConfigurator/Program.cs
+++ StarConfigurator/Program.cs
@@ -1,86 +1,32 @@
 ﻿using System;
 using System.Text;
 using System.IO;
-using Microsoft.Win32;
+using System.Configuration;
 
 namespace StarConfigurator
 {
     class Program
     {
-        static string starboundDirectory;
-        static string workshopFolder;
-        static string dirSuccess = "0";
 
         static void Main(string[] args)
         {
-            Console.WriteLine("Checking if Starbound is installed...");
-
-            findFolders();
-
-            if (dirSuccess == "1")
+            try
             {
-                try
-                {
-                    listModDirectories(workshopFolder);
-                    Console.WriteLine("Press any key to exit.");
-                    Console.ReadLine();
-                }
-                catch (Exception ex)
-                {
-                    Console.WriteLine("An unknown error occurred. Details: \n" +
-                        "\n" + ex.Message);
-                    Console.WriteLine("Press any key to exit.");
-                    Console.ReadLine();
-                }
-            }
-            else
-            {
+                string starboundDirectory = ConfigurationManager.AppSettings.Get("StarboundDirectory");
+                string workshopFolder = ConfigurationManager.AppSettings.Get("WorkshopFolder");
+                Console.WriteLine("[DEBUG] Listing mods...");
+
+                listModDirectories(workshopFolder);
+
                 Console.WriteLine("Press any key to exit.");
                 Console.ReadLine();
             }
-        }
-
-        static void findFolders()
-        {
-            try
-            {
-                var installCheck = Registry.GetValue(@"HKEY_CURRENT_USER\Software\Valve\Steam\Apps\211820", "Installed", 0);
-                if (installCheck != null)
-                {
-                    var isGameInstall = installCheck.ToString() == "1";
-                    if (isGameInstall)
-                    {
-                        Console.WriteLine("Fetching Starbound install folder...");
-
-                        string gamePath = (string)Registry.GetValue(@"HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\Steam App 211820", "InstallLocation", null);
-                        if (gamePath != null)
-                        {
-                            string sd = gamePath + @"\win64";
-                            string wd = gamePath.Replace(@"\common\Starbound", @"\workshop\content\211820");
-
-                            starboundDirectory = sd.ToString();
-                            workshopFolder = wd.ToString();
-                            dirSuccess = "1";
-                        }
-                        else
-                        {
-                            throw new Exception("Failed to find directories");
-                        }
-                    }
-                    else
-                    {
-                        Console.WriteLine("Starbound not installed.");
-                    }
-                }
-                else
-                {
-                    Console.WriteLine("Starbound possibly not installed through steam.");
-                }
-            }
             catch (Exception ex)
             {
-
-                Console.WriteLine("Unable to check for installation. " + ex.Message);
+                Console.WriteLine("An error occurred. (Maybe StarConfigurator.exe.config is missing?) Details: \n" +
+                    "\n" + ex.Message);
+                Console.WriteLine("Press any key to exit.");
+                Console.ReadLine();
             }
         }
 
@@ -95,7 +41,7 @@ namespace StarConfigurator
             foreach (string directory in directories)
             {
                 var input = (directory);
-                var output = input.Replace(workshopFolder, "");
+                var output = input.Replace(ConfigurationManager.AppSettings.Get("WorkshopFolder"), "");
 
                 sb.AppendLine("\"..\\\\..\\\\..\\\\workshop\\\\content\\\\211820\\" + output + "\\\\\",");
                 Console.WriteLine("Created entry for " + output);
@@ -113,12 +59,14 @@ namespace StarConfigurator
             sb.AppendLine("}");
             sb.AppendLine("}");
 
+            Console.WriteLine("[DEBUG] Building string...");
+
             var modList = sb.ToString();
 
             try
             {
-                Console.WriteLine("Writing file...");
-                StreamWriter sw = new StreamWriter(starboundDirectory + @"\sbinit.config");
+                Console.WriteLine("[DEBUG] Writing file...");
+                StreamWriter sw = new StreamWriter(ConfigurationManager.AppSettings.Get("StarboundDirectory") + @"\sbinit.config");
                 sw.Write(modList);
                 sw.Close();
             }
@@ -127,6 +75,8 @@ namespace StarConfigurator
                 Console.WriteLine("Could not save file. Details: \n" +
                     "\n" + ex.Message);
             }
+
+
         }
     }
 }
