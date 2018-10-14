8f5b22a181fa59507902a48ff7b4e7950ebb85b5
StreakSharpshot <jayvier13@gmail.com>
2018-10-14 05:50:36 -0500

    Updated to search for game paths

--- StarConfigurator/Program.cs
+++ StarConfigurator/Program.cs
@@ -1,32 +1,86 @@
 ﻿using System;
 using System.Text;
 using System.IO;
-using System.Configuration;
+using Microsoft.Win32;
 
 namespace StarConfigurator
 {
     class Program
     {
+        static string starboundDirectory;
+        static string workshopFolder;
+        static string dirSuccess = "0";
 
         static void Main(string[] args)
         {
-            try
+            Console.WriteLine("Checking if Starbound is installed...");
+
+            findFolders();
+
+            if (dirSuccess == "1")
             {
-                string starboundDirectory = ConfigurationManager.AppSettings.Get("StarboundDirectory");
-                string workshopFolder = ConfigurationManager.AppSettings.Get("WorkshopFolder");
-                Console.WriteLine("[DEBUG] Listing mods...");
-
-                listModDirectories(workshopFolder);
-
+                try
+                {
+                    listModDirectories(workshopFolder);
+                    Console.WriteLine("Press any key to exit.");
+                    Console.ReadLine();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("An unknown error occurred. Details: \n" +
+                        "\n" + ex.Message);
+                    Console.WriteLine("Press any key to exit.");
+                    Console.ReadLine();
+                }
+            }
+            else
+            {
                 Console.WriteLine("Press any key to exit.");
                 Console.ReadLine();
             }
+        }
+
+        static void findFolders()
+        {
+            try
+            {
+                var installCheck = Registry.GetValue(@"HKEY_CURRENT_USER\Software\Valve\Steam\Apps\211820", "Installed", 0);
+                if (installCheck != null)
+                {
+                    var isGameInstall = installCheck.ToString() == "1";
+                    if (isGameInstall)
+                    {
+                        Console.WriteLine("Fetching Starbound install folder...");
+
+                        string gamePath = (string)Registry.GetValue(@"HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\Steam App 211820", "InstallLocation", null);
+                        if (gamePath != null)
+                        {
+                            string sd = gamePath + @"\win64";
+                            string wd = gamePath.Replace(@"\common\Starbound", @"\workshop\content\211820");
+
+                            starboundDirectory = sd.ToString();
+                            workshopFolder = wd.ToString();
+                            dirSuccess = "1";
+                        }
+                        else
+                        {
+                            throw new Exception("Failed to find directories");
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Starbound not installed.");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Starbound possibly not installed through steam.");
+                }
+            }
             catch (Exception ex)
             {
-                Console.WriteLine("An error occurred. (Maybe StarConfigurator.exe.config is missing?) Details: \n" +
-                    "\n" + ex.Message);
-                Console.WriteLine("Press any key to exit.");
-                Console.ReadLine();
+
+                Console.WriteLine("Unable to check for installation. " + ex.Message);
             }
         }
 
@@ -41,7 +95,7 @@ namespace StarConfigurator
             foreach (string directory in directories)
             {
                 var input = (directory);
-                var output = input.Replace(ConfigurationManager.AppSettings.Get("WorkshopFolder"), "");
+                var output = input.Replace(workshopFolder, "");
 
                 sb.AppendLine("\"..\\\\..\\\\..\\\\workshop\\\\content\\\\211820\\" + output + "\\\\\",");
                 Console.WriteLine("Created entry for " + output);
@@ -59,14 +113,12 @@ namespace StarConfigurator
             sb.AppendLine("}");
             sb.AppendLine("}");
 
-            Console.WriteLine("[DEBUG] Building string...");
-
             var modList = sb.ToString();
 
             try
             {
-                Console.WriteLine("[DEBUG] Writing file...");
-                StreamWriter sw = new StreamWriter(ConfigurationManager.AppSettings.Get("StarboundDirectory") + @"\sbinit.config");
+                Console.WriteLine("Writing file...");
+                StreamWriter sw = new StreamWriter(starboundDirectory + @"\sbinit.config");
                 sw.Write(modList);
                 sw.Close();
             }
@@ -75,8 +127,6 @@ namespace StarConfigurator
                 Console.WriteLine("Could not save file. Details: \n" +
                     "\n" + ex.Message);
             }
-
-
         }
     }
 }
